ce329e603c9106c38f88226a79c8a18b13616f8f
Sourik <73734325+sourik10@users.noreply.github.com>
2022-07-19 18:44:21 +0530

    Create 8.cpp

---
+++ array/8.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define ll              long long int 
+#define ull             unsigned long long
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+
+void printAr(vector<int>&ar){
+	for(int i=0;i<ar.size();i++) cout<<ar[i]<<" ";
+}
+
+//better apparoch using reverseFunction 
+//time O(n),space O(1)
+void reverse(vector<int>&ar,int low,int high){
+	while(low < high){
+		swap(ar[low],ar[high]);
+		low++;
+		high--;
+	}
+}
+
+int main(){
+
+	vector<int>arr = {1,2,3,4,5,6,7};
+	int n=arr.size();
+	int k=3;
+	k=k%n;
+  
+	reverse(arr,0,k-1);
+	reverse(arr,k,n-1);
+	reverse(arr,0,n-1);
+	printAr(arr);
+
+}
+	
