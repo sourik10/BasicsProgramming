4c522965815fcda0175eb246ff94d493b649317a
Sourik <73734325+sourik10@users.noreply.github.com>
2022-09-25 09:38:44 +0530

    Update String.cpp

--- String.cpp
+++ String.cpp
@@ -1,1 +1,15 @@
-hhhhhh
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkPalindrome(string str){
+    int l=0,h=str.size()-1;
+    while(l<=h){
+        if(str[l]!=str[h]){
+            return false;
+        }
+        else{
+            l++,h--;
+        }
+    }
+    return true;
+}
