390acf317c597d10abaa87663266c6f98ef2cfc6
Sourik <73734325+sourik10@users.noreply.github.com>
2022-09-29 22:45:43 +0530

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -60,3 +60,15 @@ void rotateByK(vector<int>&v,int k){ //rotate by right
     reverse(v.begin()+k,v.end());
     reverse(v.begin(),v.end());
 }
+
+//median of an array
+double median(vector<int>&v){
+    int n=v.size();
+    sort(v.begin(),v.end());
+    if(n%2){
+        return v[n/2];
+    }
+    return (double)(v[n/2]+v[(n/2)-1])/2;
+}
+
+
